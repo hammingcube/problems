bb6be1bdc9db7a2f04f633c87e6a0067b91eca78
maddyonline <madhavjha@gmail.com>
2016-08-24 00:00:42 -0700

    more

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ import (
 	"strings"
 )
 
+type Problem struct {
+	Name      string
+	ShortDesc string
+	Desc      string
+}
+
 func main() {
 	dirname := os.Args[1]
 	files, err := ioutil.ReadDir(dirname)
@@ -16,14 +22,30 @@ func main() {
 		log.Fatal(err)
 		return
 	}
+	problems := []*Problem{}
 	for _, file := range files {
 		if file.IsDir() && strings.HasPrefix(file.Name(), "problem") {
+			problem := &Problem{
+				Name: file.Name(),
+			}
 			input, err := ioutil.ReadFile(filepath.Join(file.Name(), "README.md"))
 			if err != nil {
 				log.Fatal(err)
 				return
 			}
-			fmt.Printf("%q\n", input)
+			lines := strings.Split(string(input), "\n")
+			var i, count int
+			for i, _ = range lines {
+				if strings.HasPrefix(lines[i], "#") {
+					count += 1
+				}
+				if count == 2 {
+					break
+				}
+			}
+			problem.ShortDesc = strings.Join(lines[:i], "\n")
+			problems = append(problems, problem)
 		}
 	}
+	fmt.Printf("%#v\n", problems)
 }
