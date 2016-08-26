ccd640bbabdc63e94fec4affe92b254f8580c39a
maddyonline <madhavjha@gmail.com>
2016-08-26 01:09:23 -0700

    more

--- parse/main.go
+++ parse/main.go
@@ -6,6 +6,16 @@ import (
 	"io/ioutil"
 )
 
+func parseTemplates(tokens []markdown.Token, beg, end int) map[string]string {
+	templates := map[string]string{}
+	for _, tok := range tokens[beg:end] {
+		if tok, ok := tok.(*markdown.Fence); ok {
+			templates[tok.Params] = tok.Content
+		}
+	}
+	return templates
+}
+
 func main() {
 	md := markdown.New(markdown.XHTMLOutput(true), markdown.Nofollow(true))
 	data, _ := ioutil.ReadFile("../problem-1/README.md")
@@ -30,4 +40,18 @@ func main() {
 		}
 	}
 	fmt.Printf("%+v\n", m)
+	var templates map[string]string
+	for i, t := range m {
+		switch t.Title {
+		case "Templates":
+			var end int
+			if i == len(m)-1 {
+				end = len(parsed)
+			} else {
+				end = m[i+1].Beg
+			}
+			templates = parseTemplates(parsed, t.Beg, end)
+		}
+	}
+	fmt.Printf("%+v\n", templates)
 }
